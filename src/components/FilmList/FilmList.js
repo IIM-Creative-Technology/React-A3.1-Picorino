46784ffa36723eb7ea4ab1f4b34d6858b588d83a
Tekrodz <kevinkmmichel@gmail.com>
2023-01-31 15:22:37 +0100

    Add pagination

--- src/components/FilmList/FilmList.js
+++ src/components/FilmList/FilmList.js
@@ -6,39 +6,73 @@ class FilmList extends React.Component {
     super();
     this.state = {
       films: [],
+      pageResult: 0,
+      currentPage: 2
     };
   }
 
   componentDidMount() {
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=1bcabc0fa329a6f21493ce8ca670f65a"
-    )
-      .then((response) => response.json())
-      .then((filmsList) => {
-        this.setState({ films: filmsList["results"] });
+    this.fetchData(this.state.currentPage);
+  }
+
+  fetchData = (page) => {
+      console.log(page)
+      fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=1bcabc0fa329a6f21493ce8ca670f65a&page=" + page
+      )
+        .then((response) => response.json())
+        .then((filmsList) => {
+          this.setState({ 
+            films: filmsList["results"],
+            pageResult: filmsList["total_pages"]
+          });
+        });
+  }
+
+  handlePrevious = () => {
+    if(this.state.currentPage > 1){
+      this.setState({ 
+        currentPage: this.state.currentPage - 1
       });
+      this.fetchData(this.state.currentPage - 1)
+    }
+  }
+
+  handleNext = () => {
+    if(this.state.currentPage < this.state.pageResult){
+      this.setState({ 
+        currentPage: this.state.currentPage + 1
+      });
+      this.fetchData(this.state.currentPage + 1)
+    }
   }
 
   render() {
     return (
-      <div className="grid_two film_list">
-        {console.log(this.state.films)}
-        {this.state.films.map((film) => (
-          <div class="movie_card" key={film.id}>
-            <div class="info_section">
-              <div class="movie_header">
-                <img class="locandina" src={"https://image.tmdb.org/t/p/original/" + film.poster_path } />
-                <h1>{ film.title }</h1>
-                <h4>{ film.release_date }</h4>
-                <p class="type">Action, Crime, Fantasy</p>
+      <div>
+        <div className="grid_two film_list">
+          {this.state.films.map((film) => (
+            <div className="movie_card" key={film.id}>
+              <div className="info_section">
+                <div className="movie_header">
+                  <img className="locandina" src={"https://image.tmdb.org/t/p/original/" + film.poster_path } alt={ film.title } />
+                  <h1>{ film.title }</h1>
+                  <h4>{ film.release_date }</h4>
+                  <p className="type">Action, Crime, Fantasy</p>
+                </div>
+                <div className="movie_desc">
+                  <p className="text">{ film.overview.substring(0, 200) + '...' }</p>
+                </div>
               </div>
-              <div class="movie_desc">
-                <p class="text">{ film.overview.substring(0, 200) + '...' }</p>
-              </div>
+              <div className="blur_back back" style={{ background: `url(https://image.tmdb.org/t/p/original/` + film.poster_path + `)`}}></div>
             </div>
-            <div class="blur_back back" style={{ background: `url(https://image.tmdb.org/t/p/original/` + film.poster_path + `)`}}></div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="pagination">
+          <button onClick={ this.handlePrevious } >Previous</button>
+          <p>{ this.state.currentPage }</p>
+          <button onClick={ this.handleNext }>Next</button>
+        </div>
       </div>
     );
   }
