1a6b372b0ef99aa8a7139b7e7059849416b5f31b
Alexis Bally <alexisbally02@gmail.com>
2023-02-01 14:34:03 +0100

    change Class to Function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './views/Home';
+import CategorieFilter from './components/CategorieFilter/CategorieFilter';
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div className="App">
           <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/filter' element={<CategorieFilter/>} />
           </Routes>
       </div>
     );
