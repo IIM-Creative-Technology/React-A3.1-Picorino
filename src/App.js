e7523f48c0dc9c55c53f0180364e319e40917225
Tekrodz <kevinkmmichel@gmail.com>
2023-02-01 14:31:19 +0100

    Change class in function

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Home from './views/Home';
 
 import { Route, Routes } from 'react-router-dom';
 
-class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-          <Routes>
-            <Route path='/' element={<Home/>} />
-          </Routes>
-      </div>
-    );
-  }
+function App(){
+  return (
+    <div className="App">
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,69 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card/FilmCard";
 import './Home.css';
 
 
-class Home extends React.Component{
-    constructor() {
-        super();
-        this.state = {
-            films: [],
-            pageResult: 0,
-            currentPage: 2
-        };
-    }
+function Home(){
     
-    componentDidMount() {
-        this.fetchData(this.state.currentPage);
-    }
+    const [films, setFilms] = useState([]);
+    const [pageResult, setPageResult] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     
-    fetchData = (page) => {
+    useEffect(() => {
+        fetchData(currentPage);
+    })
+    
+    function fetchData(page){
         fetch(
             "https://api.themoviedb.org/3/discover/movie?api_key=1bcabc0fa329a6f21493ce8ca670f65a&page=" + page
         )
             .then((response) => response.json())
             .then((filmsList) => {
-                this.setState({ 
-                    films: filmsList["results"],
-                    pageResult: filmsList["total_pages"]
-                });
+                setFilms(filmsList["results"]);
+                setPageResult(filmsList["total_pages"]);
             });
     }
     
-    handlePrevious = () => {
-        if(this.state.currentPage > 1){
-            this.setState({ 
-                currentPage: this.state.currentPage - 1
-            });
-            this.fetchData(this.state.currentPage - 1)
+    function handlePrevious() {
+        if(currentPage > 1){
+            setCurrentPage(currentPage - 1)
+            fetchData(currentPage - 1)
         }
     }
     
-    handleNext = () => {
-        if(this.state.currentPage < this.state.pageResult){
-            this.setState({ 
-                currentPage: this.state.currentPage + 1
-            });
-            this.fetchData(this.state.currentPage + 1)
+    function handleNext() {
+        if(currentPage < pageResult){
+            setCurrentPage(currentPage + 1)
+            fetchData(currentPage + 1)
         }
     }
-    
-    render() {
-        return (
-            <div>
-                <div className="grid_two film_list">
-                {this.state.films.map((film) => (
-                    <Card film={film}/>
-                ))}
-                </div>
-                <div className="pagination">
-                    <button onClick={ this.handlePrevious } >Previous</button>
-                    <p>{ this.state.currentPage }</p>
-                    <button onClick={ this.handleNext }>Next</button>
-                </div>
+
+    return (
+        <div>
+            <div className="grid_two film_list">
+            { films.map((film) => (
+                <Card film={film}/>
+            ))}
             </div>
-        );
-    }
+            <div className="pagination">
+                <button onClick={ handlePrevious } >Previous</button>
+                <p>{ currentPage }</p>
+                <button onClick={ handleNext }>Next</button>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
